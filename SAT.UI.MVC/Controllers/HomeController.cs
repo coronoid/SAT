bcf8355cbb69ae61ab7569b453cdcd4d891ba2e2
= <kcchief@yahoo.com>
2021-10-14 13:30:39 -0500

    Added secret key and updated contact form to work

--- SAT.UI.MVC/Controllers/HomeController.cs
+++ SAT.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using SAT.UI.MVC.Models;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace SAT.UI.MVC.Controllers
 {
@@ -23,6 +28,51 @@ namespace SAT.UI.MVC.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string MyMessage = $"You have received an email from {cvm.Email}.  Please respond to {cvm.Email} with your response to the following message: <br/>{cvm.Message}";
+
+            MailMessage mailMessage = new MailMessage(
+            ConfigurationManager.AppSettings["EmailUser"].ToString(),
+            ConfigurationManager.AppSettings["EmailTo"].ToString(),
+            " ",
+            MyMessage);
+
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Priority = MailPriority.High;
+            mailMessage.ReplyToList.Add(cvm.Email);
+
+            SmtpClient mailClient = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+            mailClient.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+            mailClient.Port = 8889;
+
+
+
+            try
+            {
+                mailClient.Send(mailMessage);
+                ViewBag.SuccessMessage = "Thank you for contacting me, I will respond as soon as possible.";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.SuccessMessage = "Please try again later, there was a issue sending the message.";
+                ViewBag.ErrorMessage = $"We are sorry, but your request could not be completed at this time. " +
+                    $"Please try again later.  Error Message: <br/> {ex.StackTrace}";
+                return View(cvm);
+            }
 
             return View();
         }
---
+++ SAT.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SAT.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Phone")]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "* Message is required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
